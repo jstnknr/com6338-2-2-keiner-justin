461209c9acb134921f482f043406f6ceb0a34dac
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-01-23 22:05:55 -0500

    Prompts added through JS

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function greet() {
+    var username = prompt("What is your name?")
+
+    if (username && username.trim()) {
+        alert("Hello, " + username.trim())
+    } else {
+        alert('Hello')
+    }
+
+    const userAge = prompt("How old are you?")
+        console.log(parseInt(userAge));
+    
+    
+    var Birthday = window.confirm("Has your birthday passed this year? \nOK for yes or Cancel for no.");
+    if (true) {
+        alert("You were born in " + [(Date().getFullYear)-(userAge)])
+    } else  {
+        alert("You were born in " + [(Date().getFullYear)-(userAge)+1])
+    }
+}
+
